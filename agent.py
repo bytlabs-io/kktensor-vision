8b1e4d069fcbc8cd9edc765561ce670e5cd1469f
Kelvin Fiifi Nketia-Achiampong <achiampongk22@gmail.com>
2025-03-29 01:24:07 +0000

    implement for api

---
+++ agent.py
@@ -0,0 +1,139 @@
+import asyncio
+import logging
+import time
+import cv2
+import modules.globals
+from PIL import Image, ImageOps
+import modules.metadata
+
+from dotenv import load_dotenv
+
+from livekit import rtc
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    WorkerOptions,
+    cli,
+)
+from modules import core
+from modules.face_analyser import (
+    get_one_face,
+)
+from modules.processors.frame.core import get_frame_processors_modules
+
+load_dotenv()
+logger = logging.getLogger("echo-agent")
+
+PREVIEW_DEFAULT_WIDTH = 720
+PREVIEW_DEFAULT_HEIGHT = 1280
+# An example agent that echos each utterance from the user back to them
+# the example uses a queue to buffer incoming streams, and uses VAD to detect
+# when the user is done speaking.
+
+def create_webcam_preview(frame):        
+    frame_processors = get_frame_processors_modules(modules.globals.frame_processors)
+    source_image = None
+    prev_time = time.time()
+    fps_update_interval = 0.5
+    frame_count = 0
+    fps = 0
+
+    temp_frame = frame.copy()
+
+    if modules.globals.live_mirror:
+        temp_frame = cv2.flip(temp_frame, 1)
+
+    # Perform resize on frame
+
+    if source_image is None and modules.globals.source_path:
+        source_image = get_one_face(cv2.imread(modules.globals.source_path))
+
+    for frame_processor in frame_processors:
+        if frame_processor.NAME == "DLC.FACE-ENHANCER":
+            if modules.globals.fp_ui["face_enhancer"]:
+                temp_frame = frame_processor.process_frame(None, temp_frame)
+        else:
+            temp_frame = frame_processor.process_frame(source_image, temp_frame)
+
+    # Calculate and display FPS
+    current_time = time.time()
+    frame_count += 1
+    if current_time - prev_time >= fps_update_interval:
+        fps = frame_count / (current_time - prev_time)
+        frame_count = 0
+        prev_time = current_time
+
+    if modules.globals.show_fps:
+        cv2.putText(
+            temp_frame,
+            f"FPS: {fps:.1f}",
+            (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2,
+        )
+
+    # image = cv2.cvtColor(temp_frame, cv2.COLOR_BGR2RGB)
+    # image = Image.fromarray(image)
+    # image = ImageOps.contain(
+    #     image, (temp_frame.shape[1], temp_frame.shape[0]), Image.LANCZOS
+    # )
+
+    return temp_frame
+
+
+async def entrypoint(ctx: JobContext):
+    logger.info(f"connecting to room {ctx.room.name}")
+    await ctx.connect(auto_subscribe=AutoSubscribe.SUBSCRIBE_ALL)
+
+    # wait for the first participant to connect
+    participant: rtc.Participant = await ctx.wait_for_participant()
+    a_stream = rtc.AudioStream.from_participant(
+        participant=participant,
+        track_source=rtc.TrackSource.SOURCE_MICROPHONE,
+    )
+
+    v_stream = rtc.VideoStream.from_participant(
+        participant=participant,
+        track_source=rtc.TrackSource.SOURCE_CAMERA,
+    )
+
+    a_source = rtc.AudioSource(sample_rate=48000, num_channels=1)
+    v_source = rtc.VideoSource(width=PREVIEW_DEFAULT_WIDTH, height=PREVIEW_DEFAULT_HEIGHT)
+    a_track = rtc.LocalAudioTrack.create_audio_track("echo-a", a_source)
+    v_track = rtc.LocalVideoTrack.create_video_track("echo-v", v_source)
+    await ctx.room.local_participant.publish_track(
+        a_track,
+        rtc.TrackPublishOptions(source=rtc.TrackSource.SOURCE_MICROPHONE),
+    )
+    await ctx.room.local_participant.publish_track(
+        v_track,
+        rtc.TrackPublishOptions(source=rtc.TrackSource.SOURCE_CAMERA),
+    )
+
+    async def _process_audio():
+        async for ev in a_stream:
+            # delay for 100ms fo It can sync with video, this should be programmable for the user
+            await asyncio.sleep(0.2)
+            await a_source.capture_frame(ev.frame)
+
+    async def _process_video():
+        async for ev in v_stream:
+            v_source.capture_frame(create_webcam_preview(ev.frame))
+
+
+    await asyncio.gather(
+        _process_video(),
+        _process_audio()
+    )
+
+if __name__ == '__main__':
+    core.run()
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+        ),
+    )
--- modules/core.py
+++ modules/core.py
@@ -17,9 +17,8 @@ import tensorflow
 
 import modules.globals
 import modules.metadata
-import modules.ui as ui
 from modules.processors.frame.core import get_frame_processors_modules
-from modules.utilities import has_image_extension, is_image, is_video, detect_fps, create_video, extract_frames, get_temp_frame_paths, restore_audio, create_temp, move_temp, clean_temp, normalize_output_path
+from modules.utilities import clean_temp, normalize_output_path
 
 if 'ROCMExecutionProvider' in modules.globals.execution_providers:
     del torch
@@ -172,70 +171,12 @@ def pre_check() -> bool:
 
 def update_status(message: str, scope: str = 'DLC.CORE') -> None:
     print(f'[{scope}] {message}')
-    if not modules.globals.headless:
-        ui.update_status(message)
 
 def start() -> None:
     for frame_processor in get_frame_processors_modules(modules.globals.frame_processors):
         if not frame_processor.pre_start():
             return
     update_status('Processing...')
-    # process image to image
-    if has_image_extension(modules.globals.target_path):
-        if modules.globals.nsfw_filter and ui.check_and_ignore_nsfw(modules.globals.target_path, destroy):
-            return
-        try:
-            shutil.copy2(modules.globals.target_path, modules.globals.output_path)
-        except Exception as e:
-            print("Error copying file:", str(e))
-        for frame_processor in get_frame_processors_modules(modules.globals.frame_processors):
-            update_status('Progressing...', frame_processor.NAME)
-            frame_processor.process_image(modules.globals.source_path, modules.globals.output_path, modules.globals.output_path)
-            release_resources()
-        if is_image(modules.globals.target_path):
-            update_status('Processing to image succeed!')
-        else:
-            update_status('Processing to image failed!')
-        return
-    # process image to videos
-    if modules.globals.nsfw_filter and ui.check_and_ignore_nsfw(modules.globals.target_path, destroy):
-        return
-
-    if not modules.globals.map_faces:
-        update_status('Creating temp resources...')
-        create_temp(modules.globals.target_path)
-        update_status('Extracting frames...')
-        extract_frames(modules.globals.target_path)
-
-    temp_frame_paths = get_temp_frame_paths(modules.globals.target_path)
-    for frame_processor in get_frame_processors_modules(modules.globals.frame_processors):
-        update_status('Progressing...', frame_processor.NAME)
-        frame_processor.process_video(modules.globals.source_path, temp_frame_paths)
-        release_resources()
-    # handles fps
-    if modules.globals.keep_fps:
-        update_status('Detecting fps...')
-        fps = detect_fps(modules.globals.target_path)
-        update_status(f'Creating video with {fps} fps...')
-        create_video(modules.globals.target_path, fps)
-    else:
-        update_status('Creating video with 30.0 fps...')
-        create_video(modules.globals.target_path)
-    # handle audio
-    if modules.globals.keep_audio:
-        if modules.globals.keep_fps:
-            update_status('Restoring audio...')
-        else:
-            update_status('Restoring audio might cause issues as fps are not kept...')
-        restore_audio(modules.globals.target_path, modules.globals.output_path)
-    else:
-        move_temp(modules.globals.target_path, modules.globals.output_path)
-    # clean and validate
-    clean_temp(modules.globals.target_path)
-    if is_video(modules.globals.target_path):
-        update_status('Processing to video succeed!')
-    else:
-        update_status('Processing to video failed!')
 
 
 def destroy(to_quit=True) -> None:
@@ -252,8 +193,5 @@ def run() -> None:
         if not frame_processor.pre_check():
             return
     limit_resources()
-    if modules.globals.headless:
-        start()
-    else:
-        window = ui.init(start, destroy, modules.globals.lang)
-        window.mainloop()
+    start()
+ 
